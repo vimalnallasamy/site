f13d1a8d3e7568515a88cef683a3753b42a7c5a5
Vimal Kumar <vimalkumar.nallasamy@siriuscom.com>
2021-06-14 18:36:45 +0530

    initial commit

---
+++ src/components/about.jsx
@@ -0,0 +1,108 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-about" data-section="about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-12">
+                <div className="row row-bottom-padded-sm animate-box" data-animate-effect="fadeInLeft">
+                <div className="col-md-12">
+                    <div className="about-desc">
+                    <span className="heading-meta">About Us</span>
+                    <h2 className="colorlib-heading">Who Am I?</h2>
+                    <p>An enthusiastic designer and developer with a big passion for Web and Mobile Applications. Very professional with a great work ethic and an honest worker who always focuses on the best results. </p>
+                    <p>Technically, I code with HTML5, CSS3 and Javascript, using Angular as the main framework. On the mobile part, I am actually in love with Cordova & Ionic. I am also equally good in JAVA, J2EE and other technologies (Look into my skill set). Additionally, I also have a great love for Learning. </p>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </section>
+        <section className="colorlib-about">
+        <div className="colorlib-narrow-content">
+            <div className="row">
+            <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">What I do?</span>
+                <h2 className="colorlib-heading">Here are some of my expertise</h2>
+            </div>
+            </div>
+            <div className="row row-pt-md">
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-1">
+                <span className="icon">
+                    <i className="icon-bulb" />
+                </span>
+                <div className="desc">
+                    <h3>Web Development </h3>
+                    <p>I have experience building websites using JavaScript, React, Angular, jQuery, HTML, CSS.</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-5">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Mobile Apps</h3>
+                    <p>I have wide range of experience in building hybrid and native mobile apps for Android and iOS</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-3">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Backend Development</h3>
+                    <p>I have good experience in developing effective services for the UI to consume </p>
+                </div>
+                </div>
+            </div>
+            {/*
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-2">
+                <span className="icon">
+                    <i className="icon-data" />
+                </span>
+                <div className="desc">
+                    <h3>Dev Ops</h3>
+                    <p>Jenkins , Kubernetes , Docker </p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-4">
+                <span className="icon">
+                    <i className="icon-layers2" />
+                </span>
+                <div className="desc">
+                    <h3>Graphic Design</h3>
+                    <p>My friend knows .. P</p>
+                </div>
+                </div>
+            </div>
+            <div className="col-md-4 text-center animate-box">
+                <div className="services color-6">
+                <span className="icon">
+                    <i className="icon-phone3" />
+                </span>
+                <div className="desc">
+                    <h3>Digital Marketing</h3>
+                    <p>I use Instagram eight hours a day :) </p>
+                </div>
+                </div>
+            </div>
+            */}
+            </div>
+        </div>
+        </section>
+      </div>
+    )
+  }
+}
