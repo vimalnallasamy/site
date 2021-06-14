8def8c5ae55167e8b6884bfd12f2e57cb0ffe655
Vimal Kumar <vimalkumar.nallasamy@siriuscom.com>
2021-06-14 18:39:19 +0530

    Updates

---
+++ precache-manifest.e09f7ce4208a421236e31b67a9b1e150.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5ddfea6404917915c4c",
+    "url": "/me/static/css/main.68a757e2.chunk.css"
+  },
+  {
+    "revision": "b5ddfea6404917915c4c",
+    "url": "/me/static/js/main.b5ddfea6.chunk.js"
+  },
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "77e6fabf010424b218db34d16c66cb12",
+    "url": "/me/index.html"
+  }
+];
